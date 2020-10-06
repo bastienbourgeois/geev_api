c832d9b95b36ce24c912f59815ba3230e9de9e25
Bastien <banebps@hotmail.fr>
2020-10-06 20:32:07 +0200

    get object conv update

--- src/geev.py
+++ src/geev.py
@@ -1,8 +1,10 @@
 import requests
-import json
+
+from bs4 import BeautifulSoup
 
 LOGIN_URL = 'https://prod.geev.fr/api/v0.19/user/auth/local/login'
 CONV_URL = 'https://prod.geev.fr/api/v0.19/user/self/conversations?limit=true'
+OBJ_URL = 'https://www.geev.com/fr/recherche/objets'
 
 class Geev:
     session = any
@@ -17,16 +19,25 @@ class Geev:
         payload = {
             'login_id': username,
             'password': password
-            }
-        r = self.session.post(LOGIN_URL, data=json.dumps(payload)).json()
+        }
+        r = self.session.post(LOGIN_URL, data=payload).json()
         self.id = r['user']['_id']
         self.app_token = r['app_token']
-    
+
     def getConversation(self):
-        header = {'content-type': 'application/json'}
         payload = {
             'x-geev-token': self.app_token
         }
-        r = self.session.get(CONV_URL, params=payload, headers=header).json()
-        print(r)
+        r = self.session.get(CONV_URL, headers=payload).json()
+
+    def getObjects(self, page, location, type, distance):
+        url = OBJ_URL + '?page=' + str(page) + '&location=' + location
+        url += '&type=' + type + '&distance=' + str(distance)
         
+        r = self.session.get(url)
+        soup = BeautifulSoup(r.content, 'html.parser')
+        #soup = soup.find_all("div", class_="mol-items-panel")
+        soup = soup.find_all("a", class_="mol-itemCard")
+        for s in soup:
+            print (s['href'])
+        #print(soup)
