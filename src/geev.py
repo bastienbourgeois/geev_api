fca784280d97ab26b0a016b423ed5acf6ec735f6
Bastien <banebps@hotmail.fr>
2020-10-07 22:43:14 +0200

    getObject ok

--- src/geev.py
+++ src/geev.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 LOGIN_URL = 'https://prod.geev.fr/api/v0.19/user/auth/local/login'
 CONV_URL = 'https://prod.geev.fr/api/v0.19/user/self/conversations?limit=true'
 OBJ_URL = 'https://www.geev.com/fr/recherche/objets'
+ADDRESS_URL = 'https://prod.geev.fr/api/v0.19/autocomplete'
 
 class Geev:
     session = any
@@ -24,22 +25,52 @@ class Geev:
         self.id = r['user']['_id']
         self.app_token = r['app_token']
 
+    def getLocationFromAddress(self, address):
+        url = ADDRESS_URL + "?address=" + address
+        r = self.session.get(url).json()
+        location = str(r[0]['location']['latitude']) + "%2C"
+        location += str(r[0]['location']['longitude'])
+        if r == None:
+            print('Address not found')
+            return None
+        return location
+
     def getConversation(self):
         payload = {
             'x-geev-token': self.app_token
         }
         r = self.session.get(CONV_URL, headers=payload).json()
+        print(r)
 
-    def getObjects(self, page, location, type, distance):
-        obj = []
+    def getObjects(self, page, location, distance):
+        objs = []
+        if (location == None):
+            print('Location error')
+            return None
         url = OBJ_URL + '?page=' + str(page) + '&location=' + location
-        url += '&type=' + type + '&distance=' + str(distance)
+        url += '&type=donation&distance=' + str(distance)
         r = self.session.get(url)
         soup = BeautifulSoup(r.content, 'html.parser')
         soup = soup.find_all("a", class_="mol-itemCard")
         for s in soup:
             if (s['href'][:4] == "/fr/"):
+                obj = []
                 obj.append(s['href'])
-                print(s['href'])
-        return obj
-        #print(soup)
+                split = s['href'].split('/')
+                obj.append(split[4])
+                obj.append(split[3])
+                params = s.find("ul", class_="mol-itemCard-description-info").select('li')
+                obj.append(params[1].text[:-1])
+                obj.append(params[0].text)
+                dispo = s.find("div", class_="mol-itemCard-cover-availability")
+                if dispo == None:
+                    obj.append('Available')
+                else:
+                    obj.append(dispo.text[1:-1])
+                bannane = s.find("span", class_="mol-itemCard-cover-banner-text")
+                if bannane == None:
+                    obj.append('1bannane')
+                else:
+                    obj.append(bannane.text[1:])
+                objs.append(obj)
+        return objs
